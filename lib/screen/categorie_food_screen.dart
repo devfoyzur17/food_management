56bb8943e3f232c7797340c9af1efa79e0e0bfc7
foyzur17 <foyzur17@gmail.com>
2022-05-19 08:29:34 +0600

    delate ieam

--- lib/screen/categorie_food_screen.dart
+++ lib/screen/categorie_food_screen.dart
@@ -2,10 +2,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:food_management/data/food_data.dart';
+import 'package:food_management/model/food_model.dart';
 import 'package:food_management/widget/food_item.dart';
 
-class CategorieFoodScreen extends StatelessWidget {
-  // final String categorieId; 
+class CategorieFoodScreen extends StatefulWidget {
+  // final String categorieId;
   // final String categorieTittle;
   // const CategorieFoodScreen(
   //     {Key? key, required this.categorieId, required this.categorieTittle})
@@ -14,26 +15,61 @@ class CategorieFoodScreen extends StatelessWidget {
   static const routeName = '/categorie-food-screen';
 
   @override
+  State<CategorieFoodScreen> createState() => _CategorieFoodScreenState();
+}
+
+class _CategorieFoodScreenState extends State<CategorieFoodScreen> {
+  String? categoryTitle;
+  List<FoodModel>? displayedFood;
+  bool isLodding = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+
+    if (!isLodding) {
+      final routArgs =
+          ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+      final categorieId = routArgs['id'];
+      categoryTitle = routArgs['tittle'];
+      displayedFood = foodData.where((food) {
+        return food.categories.contains(categorieId);
+      }).toList();
+      isLodding = true;
+    }
+
+    super.didChangeDependencies();
+  }
+
+  void removeFood(String foodId) {
+    setState(() {
+      displayedFood!.removeWhere((element) => element.id == foodId);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final routArgs =
-        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
-    final categorieId = routArgs['id'];
-    final categorieTitle = routArgs['tittle'];
-    final categorieFood = foodData.where((food) {
-      return food.categories.contains(categorieId);
-    }).toList();
     return Scaffold(
-      appBar: AppBar(title: Text(categorieTitle!)),
+      appBar: AppBar(title: Text(categoryTitle!)),
       body: ListView.builder(
-          itemCount: categorieFood.length,
+          itemCount: displayedFood!.length,
           itemBuilder: (context, index) {
             return FoodItem(
-              id: categorieFood[index].id,
-                tittle: categorieFood[index].title,
-                imageUrl: categorieFood[index].imageUrl,
-                duration: categorieFood[index].duration,
-                complexity: categorieFood[index].complexity,
-                affordability: categorieFood[index].affordability);
+              id: displayedFood![index].id,
+              tittle: displayedFood![index].title,
+              imageUrl: displayedFood![index].imageUrl,
+              duration: displayedFood![index].duration,
+              complexity: displayedFood![index].complexity,
+              affordability: displayedFood![index].affordability,
+              removeItem: removeFood,
+            );
           }),
     );
   }
